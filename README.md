bebf79dc7e0c6d5795c217c51984e99ea7cd13a2
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-07 15:35:41 +0600

    project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# user-managemnet-client
---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
+
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email}
+    console.log(user);
+  }
+
+  return (
+    <>
+      <h1>User management system: {users.length}</h1>
+
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+      />
+      <br />
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+      />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+
+
+      {
+        users.map(user => <p key={user.id}>name: {user.name} : {user.email}</p>)
+      }
+    </>
+  )
+}
+
+export default App
